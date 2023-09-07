d2bd8a8d68b6363782f1946abfd3502758970b5b
Doron Levy <dolevy@g.jct.ac.il>
2023-09-07 19:47:22 +0300

    imageGUI

---
+++ WpfApp1/Model/DailyPictureModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DATA_CLASSES;
+using API.services.implementation;
+using Microsoft.Extensions.Configuration;
+using System.Configuration;
+using System.Net.Http;
+
+namespace GUI.Model
+{
+    internal class DilyPictureModel
+    {
+        NasaDailyImageResponse NasaDailyImageResponse { get; set; }
+        NasaService nasaService { get; set; }
+
+        public DilyPictureModel() {
+            nasaService = new NasaService();
+            NasaDailyImageResponse = NasaService.GetDailyImage();
+        }
+    }
+}
---
+++ WpfApp1/ViewModel/DailyPictureVM.cs
@@ -0,0 +1,34 @@
+﻿using DATA_CLASSES;
+using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using API.Controllers;
+
+namespace GUI.ViewModel
+{
+    class DailyPictureVM : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+
+
+        internal NasaDailyImageResponse? imageResponse;
+
+        internal MediaElement? dailyPicture;
+
+        DailyPictureVM()
+        {
+            imageResponse = ;
+
+            dailyPicture = new MediaElement();
+            dailyPicture.Source = new Uri();
+
+        }
+
+    }
+}
