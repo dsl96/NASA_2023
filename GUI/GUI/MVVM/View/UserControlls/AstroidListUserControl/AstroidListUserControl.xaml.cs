4dda9fed5cc17ad3cf51178c1f1cb46ab360c9ca
Doron Levy <dolevy@g.jct.ac.il>
2023-10-15 16:10:37 +0300

    finish all whitout the scroll viewer speed control

--- GUI/GUI/MVVM/View/UserControlls/AstroidListUserControl/AstroidListUserControl.xaml.cs
+++ GUI/GUI/MVVM/View/UserControlls/AstroidListUserControl/AstroidListUserControl.xaml.cs
@@ -4,14 +4,17 @@ using System.ComponentModel;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
+using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
+using System.Windows.Input;
+using System.Windows.Media.Animation;
 
 namespace GUI.MVVM.View.UserControlls
 {
     public partial class AstroidListUserControl : UserControl
     {
-        private readonly AstroidListVM vm;  
+        private readonly AstroidListVM vm;
         public ICollectionView CollectionView { get; set; }
 
         public AstroidListUserControl()
